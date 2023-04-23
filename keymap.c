0197f7ea66f0c9bf873a88ab1a679901a591dcdd
drevilbones <drevilbones@gmail.com>
2023-04-23 00:05:21 -0400

    updating to current QMK

--- keymap.c
+++ keymap.c
@@ -2,38 +2,32 @@
 
 led_flags_t current_flags;
 
-void set_rgb_default(void) {
-    rgb_matrix_mode(RGB_MATRIX_STARTUP_MODE);
-    rgb_matrix_set_speed(RGB_MATRIX_STARTUP_SPD);
-    //decrease underglow brightness
-}
-
 void keyboard_post_init_user() {
     usb_extra_manual = 1; //turn on second usb port
-    set_rgb_default();
 }
 
 enum alt_keycodes {
     MD_BOOT = SAFE_RANGE,   //Restart into bootloader after hold timeout
-    RGB_DEF,                //Set RGB mode to the default/startup
-    UNG_BRI,                //increase underglow brightness
-    UNG_BRD                 //decrease underglow brightness
+    UND_VAI,                //increase underglow brightness
+    UND_VAD                 //decrease underglow brightness
 };
+
+//TODO: fix underglow control
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT_65_ansi_blocker(
-        KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_DEL,  \
-        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_INS, \
+        QK_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_DEL,  \
+        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, DM_PLY1, \
         KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,  KC_PGUP, \
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,          KC_UP,   KC_PGDN, \
         KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                             KC_RCTL, MO(1),   KC_LEFT, KC_DOWN, KC_RGHT  \
     ),
     [1] = LAYOUT_65_ansi_blocker(
-        KC_TILD, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, KC_MUTE, \
-        RGB_DEF, RGB_SPD, RGB_VAI, RGB_SPI, RGB_HUI, RGB_SAI, UNG_BRI, _______, KC_PSCR, KC_SLCK, KC_PAUS, _______, _______, _______, KC_MSTP, \
-        _______, RGB_RMOD,RGB_VAD, RGB_MOD, RGB_HUD, RGB_SAD, UNG_BRD, _______, _______, _______, _______, _______,         A(KC_ENT),KC_VOLU, \
-        _______, RGB_TOG,A(KC_F4), _______, _______, MD_BOOT, NK_TOGG, _______, KC_MPRV, KC_MNXT, _______, _______,          KC_APP,  KC_VOLD, \
-        _______, _______, _______,                            KC_MPLY,                            _______, _______, KC_HOME, KC_LOCK, KC_END  \
+        KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, KC_MUTE, \
+        _______, RGB_SPD, RGB_VAI, RGB_SPI, UND_VAI, _______, _______, _______, KC_PSCR, KC_SCRL, KC_PAUS, _______, _______, _______, DM_REC1, \
+        _______, RGB_RMOD,RGB_VAD, RGB_MOD, UND_VAD, _______, _______, _______, _______, _______, _______, _______,         A(KC_ENT),KC_VOLU, \
+        _______, RGB_TOG,A(KC_F4), _______, _______, MD_BOOT, _______, _______, KC_MPRV, KC_MNXT, _______, _______,          KC_APP,  KC_VOLD, \
+        _______, _______, _______,                            KC_MPLY,                            _______, _______, KC_HOME, QK_LOCK, KC_END  \
     ),
     /*
     [X] = LAYOUT(
@@ -63,15 +57,14 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                 }
             }
             return false;
-        case RGB_DEF: //reset RBG settings to startup
-            set_rgb_default();
+        case UND_VAI:
+            rgb_matrix_set_flags(LED_FLAG_UNDERGLOW);
+            rgb_matrix_increase_val();
             return false;
-        case UNG_BRI:
-            rgblight_increase_val();
+        case UND_VAD:
+            rgb_matrix_set_flags(LED_FLAG_UNDERGLOW);
+            rgb_matrix_decrease_val();
             return false;
-        case UNG_BRD:
-            rgblight_decrease_val();
-            return false;            
         case RGB_TOG:
             if (record->event.pressed) {
               switch (rgb_matrix_get_flags()) {
